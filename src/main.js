4a9182f5a7841c084f1c51089d5524aad31d3695
yaofeiyu <1048881641@qq.com>
2023-06-08 13:58:02 +0800

    second commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router.js'
 import './plugins/element.js'
+// import './assets/fonts/iconfont.css'
+
+//导入全局样式表
+import './assets/css/global.css'
+
+import axios from 'axios'
+//配置请求根路径
+axios.defaults.baseURL = 'http://43.143.0.76:8889/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,15 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// import { Button } from 'element-ui'
+// import {Form, FormItem} from 'element-ui'
+// import {Input} from 'element-ui'
+// import { Message } from 'element-ui'
 
+import {Button, Form, FormItem, Input, Message} from 'element-ui'
+
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 Vue.use(Button)
+
+//全局挂载
+Vue.prototype.$message = Message
